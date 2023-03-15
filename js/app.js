83212f637b3993b1a86bd65a4ad04ead9dea9aef
v01010v <jbeutz@gmail.com>
2023-03-15 06:56:36 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,24 +6,33 @@ $(document).ready(function() {
     success: function(data) {
       // Transform the JSON data into an array of objects
       var dataArray = [];
-      for (var i = 0; i < data.length; i++) {
-        var rowData = data[i];
-        var rowArray = [];
-        for (var j = 0; j < rowData.length; j++) {
-          rowArray.push(rowData[j]);
-        }
-        dataArray.push(rowArray);
+      for (var i = 0; i < data.data.length; i++) {
+        var rowData = data.data[i];
+        var rowObject = {
+          "dateDeath": rowData.dateDeath,
+          "nameFirst": rowData.nameFirst,
+          "nameLast": rowData.nameLast,
+          "nokFirst": rowData.nokFirst,
+          "nokLast": rowData.nokLast,
+          "nokType": rowData.nokType,
+          "nokEmail": rowData.nokEmail
+        };
+        dataArray.push(rowObject);
       }
 
       // Initialize DataTables with the transformed data
       $('#myTable').DataTable({
         data: dataArray,
         columns: [
-          { title: 'Column 1' },
-          { title: 'Column 2' }
+          { title: 'Date of Death', data: 'dateDeath' },
+          { title: 'First Name', data: 'nameFirst' },
+          { title: 'Last Name', data: 'nameLast' },
+          { title: 'NOK First', data: 'nokFirst' },
+          { title: 'NOK Last', data: 'nokLast' },
+          { title: 'NOK Type', data: 'nokType' },
+          { title: 'NOK Email', data: 'nokEmail' }
         ]
       });
     }
   });
 });
-
